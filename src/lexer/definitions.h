58b49aba4779661112ede446afd200d17545a1ba
Kairav <kairavdutta@gmail.com>
2026-07-13 04:11:50 +0530

    lexer: added constructors to the structs

--- src/lexer/definitions.h
+++ src/lexer/definitions.h
@@ -10,8 +10,11 @@
 struct Parameter{
     std::string dataType;
     std::optional<std::string> param_name;
-    Parameter(std::string type, std::string name){
+    Parameter(std::string type){
         this->dataType = type;
+    }
+    Parameter(std::string type, std::string name){
+        this->dataType   = type;
         this->param_name = name;
     }
 };
@@ -27,28 +30,66 @@ struct Function{
     std::vector<Parameter> params;
     std::string returnType;
     uint8_t flags;
+    Function(std::string name, 
+             std::vector<Parameter> params,
+             std::string returnType
+            ){
+        this->name       = name;
+        this->params     = std::move(params);
+        this->returnType = returnType;
+    }
 };
 
 struct EnumField{
     std::string name;
     std::optional<std::string> value;
+    EnumField(std::string name, std::string value){
+        this->name  = name;
+        this->value = value;
+    }
 };
 
 struct Enum{
     std::string name;
     std::vector<EnumField> fields;
+    Enum(std::string name, std::vector<EnumField> fields){
+        this->name   = name;
+        this->fields = std::move(fields);
+    }
 };
 
 struct ExternVariable{
     std::string type;
     std::string name;
+    ExternVariable(std::string type,
+                   std::string name
+                  ){
+        this->type = type;
+        this->name = name;
+    }
+};
+
+struct StructField{
+    std::string type;
+    std::string name;
+    StructField(std::string type,
+                std::string name                
+               ){
+        this->type = type;
+        this->name = name;
+    }
 };
 
 struct Struct{
     std::string name;
-    std::string fields; // as C structs have no initalization
+    std::vector<StructField> fields; // as C structs have no initalization
+    Struct(std::string name,
+           std::vector<StructField> fields
+          ){
+        this->name   = name;
+        this->fields = std::move(fields);
+    }
 };
-
 
 
 
